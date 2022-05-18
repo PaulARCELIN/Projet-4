6be9d88bf136b4e1f538ab9f217e1476b4f31136
Paul <paularcelin@hotmail.fr>
2022-05-18 13:42:56 +0200

    Ajouts des premiers fichiers

---
+++ modal.js
@@ -0,0 +1,35 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+// Ajout bouton close
+const closeBtn = document.querySelectorAll(".close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+
+
+// close modal event
+closeBtn.forEach((elt) => elt.addEventListener("click", closeModal));
+
+//close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
